233bb0eea77dc93fa758f49530a89b84bba64dd9
Duff <59646857+apizdose@users.noreply.github.com>
2022-11-02 12:12:29 +0200

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,4 +1,4 @@
 #Generate ssl certs
 
-sudo openssl req -x509 -nodes -days 365000 -newkey rsa:2048 -keyout ./nginx-selfsigned.key -out ./nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365000 -newkey rsa:2048 -keyout ./nginx-selfsigned.key -out ./nginx-selfsigned.crt -subj "/C=SI/ST=selfsigned/L=selfsigned/O=selfsigned/OU=selfsigned Department/CN=www.selfsigned.com"
 
